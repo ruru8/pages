52cefda1188b01a08caafb4619994439588b9447
ruru8 <chiharu.yasuda@classi.jp>
2020-10-25 15:30:02 +0900

    add fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,21 @@ export default {
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     '@nuxt/content',
+    'nuxt-fontawesome'
   ],
+
+  fontawesome: {
+    imports: [
+      {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas']
+      },
+      {
+        set: '@fortawesome/free-brands-svg-icons',
+        icons: ['fab']
+      }
+    ]
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
