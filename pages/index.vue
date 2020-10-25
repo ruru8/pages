51b5062e1e370eadc6df5ea6eb192f4af99a3426
ruru8 <chiharu.yasuda@classi.jp>
2020-10-25 18:29:23 +0900

    add height

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container class="top">
     <div class="d-flex justify-content-center align-content-center py-4">
-      <img src="~/assets/ruru8.png" width="250px" />
+      <img src="~/assets/ruru8.png" width="250px"  height="250px" />
     </div>
     <div class="profile text-center ">
       <div>ruru8</div>
